9491e819afbaf3dda8ae266b11a93b5863105ef3
mikoi-ondi <124450553+mikoi-ondi@users.noreply.github.com>
2023-05-17 20:36:16 +0300

    Add files via upload
    общая страница логина и регистрации. редиректы. хеширование пароля. проверка корректности email

---
+++ controller/login.php
@@ -0,0 +1,22 @@
+<?php
+include 'config.php';
+
+session_start();
+if (isset($_POST['email']) and isset($_POST['password'])) {
+    //global $conn;
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT * FROM users WHERE email = '$email' and password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    $user = mysqli_fetch_assoc($result);
+
+    if (isset($user)) {
+        $_SESSION['auth'] = true;
+        
+        header("Location: /view/feedback.html");
+    } else {
+        echo("Пользователь не найден");
+    }
+
+}
