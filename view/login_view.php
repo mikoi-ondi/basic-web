3895e4c3e6ea07dbc1709a2b48c7b94df052d203
mikor-ondi <thinkundlove@yandex.ru>
2023-05-19 20:11:59 +0400

    добавил тайтлы

--- view/login_view.php
+++ view/login_view.php
@@ -2,11 +2,12 @@
     <!doctype html>
 <html lang="ru">
 <head>
+    
   <!-- Кодировка веб-страницы -->
   <meta charset="utf-8">
   <!-- Настройка viewport -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>...</title>
+  <title> Авторизация </title>
 
   <!-- Bootstrap CSS (jsDelivr CDN) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
